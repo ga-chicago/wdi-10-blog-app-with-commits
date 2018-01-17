9d562d5649c388dd00f589ac635a4c8f25c38bb8
Reuben Ayres <ra@sent.com>
2018-01-17 16:43:00 -0600

    article create route

--- controllers/articleController.js
+++ controllers/articleController.js
@@ -1,12 +1,20 @@
 const express = require('express');
 const router = express.Router();
 
+const Article = require('../models/article.js');
+
 router.get('/', (req, res)=>{
 	res.render('articles/index.ejs');
 });
 
 router.get('/new', (req, res)=>{
   res.render('articles/new.ejs');
+});
+
+router.post('/', (req, res)=>{
+  Article.create(req.body, (err, createdArticle)=>{
+    res.redirect('/articles');
+  });
 });
 
 module.exports = router;
