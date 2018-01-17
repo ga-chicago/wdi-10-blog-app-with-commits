6437884ef1c34490bea85e16fa69990240bc1553
Reuben Ayres <ra@sent.com>
2018-01-17 16:15:06 -0600

    author index

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const app = express();
 
-app.get('/', (req, res)=>{
+const authorController = require('./controllers/authorController.js');
+app.use('/authors', authorController);
+
+app.get('/', (req, res) => {
   res.render('index.ejs');
 });
 
